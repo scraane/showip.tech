8508a6d512e0e89aa51fe1e51b46cff5a8f84c5a
nutjob <stephan.craane@gmail.com>
2022-04-06 21:30:41 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require_once("geoip2.phar");
 use GeoIp2\Database\Reader;
 // City DB
-
+$reader = new Reader('GeoLite2-City.mmdb');
 
 
 
@@ -10,6 +10,25 @@ use GeoIp2\Database\Reader;
 $userIP = getRealIP();
 $locationIP = getLocationData($userIP);
 echo $userIP.'<br/>';
+$country_code = $locationIP['country_code']; 
+$country_name = $locationIP['country_name']; 
+$region_code = $locationIP['region_code']; 
+$region_name = $locationIP['region_name']; 
+$city = $locationIP['city']; 
+$zip_code = $locationIP['postal_code']; 
+$latitude = $locationIP['latitude']; 
+$longitude = $locationIP['longitude']; 
+$time_zone = $locationIP['time_zone']; 
+
+echo 'Country Name: '.$country_name.'<br/>'; 
+echo 'Country Code: '.$country_code.'<br/>'; 
+echo 'Region Code: '.$region_code.'<br/>'; 
+echo 'Region Name: '.$region_name.'<br/>'; 
+echo 'City: '.$city.'<br/>'; 
+echo 'Zipcode: '.$zip_code.'<br/>'; 
+echo 'Latitude: '.$latitude.'<br/>'; 
+echo 'Longitude: '.$longitude.'<br/>'; 
+echo 'Time Zone: '.$time_zone;
 
 function getRealIP() {  
     if (!empty($_SERVER['HTTP_CLIENT_IP']))   
@@ -31,17 +50,16 @@ function getRealIP() {
 
 
 function getLocationData($userIP) {
-    $reader = new Reader('GeoLite2-City.mmdb');
     $record = $reader->city($userIP);
-    print($record->country->isoCode . "\n");
-    print($record->country->name . "\n");
-    print($record->country->names['zh-CN'] . "\n");
-    print($record->mostSpecificSubdivision->name . "\n");
-    print($record->mostSpecificSubdivision->isoCode . "\n");
-    print($record->city->name . "\n");
-    print($record->postal->code . "\n");
-    print($record->location->latitude . "\n");
-    print($record->location->longitude . "\n");
+    $locationIP['country_code'] = $record->country->isoCode;
+    $locationIP['country_name'] = $record->country->name;
+    $locationIP['region_name'] = $record->mostSpecificSubdivision->name;
+    $locationIP['region_code'] = $record->mostSpecificSubdivision->isoCode;
+    $locationIP['city'] = $record->city->name;
+    $locationIP['postal_code'] = $record->postal->code;
+    $locationIP['latitude'] = $record->location->latitude;
+    $locationIP['longitude'] = $record->location->longitude;
+    $locationIP['time_zone'] = $record->time->zone
     
     if(!empty($locationIP)){ 
         return $locationIP;
